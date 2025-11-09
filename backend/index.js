6dd75b43e2e468ed50e727fcd51d2d9a3eaa8d39
Bijay kumar shah <bjsah3277@gmail.com>
2025-11-10 00:58:04 +0530

    Refactor CORS middleware to specify allowed origins and enable credentials

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,10 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175','https://travel-karnataka.onrender.com'],
+  credentials: true
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
